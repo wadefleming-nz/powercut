01df73e67dc13e6551a203107cee3de56382bb05
Wade Fleming <WFleming@caci.co.uk>
2020-05-16 22:03:24 +0100

    setup firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,25 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { firebaseConfig } from './firebase.config';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyD0VWO1P-rih5EkwyqSsI32nUwFIeeoNqQ',
+  authDomain: 'power-cut.firebaseapp.com',
+  databaseURL: 'https://power-cut.firebaseio.com',
+  projectId: 'power-cut',
+  storageBucket: 'power-cut.appspot.com',
+  messagingSenderId: '490056093450',
+  appId: '1:490056093450:web:24eacc37e32f235cc3f4f3',
+  measurementId: 'G-8H5DZ0NYPM',
+};
