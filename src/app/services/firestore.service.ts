d2647dc2368d6d2222e6978ec9577162d4242109
Wade Fleming <WFleming@caci.co.uk>
2020-05-16 20:56:35 +0100

    added firestore service

---
+++ src/app/services/firestore.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/firestore';
+import { Incident } from 'src/app/models/incident';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FirestoreService {
+  incidentListPath = 'incidentList';
+  incidentCollection: AngularFirestoreCollection<Incident>;
+
+  constructor(public firestore: AngularFirestore) {
+    this.incidentCollection = this.firestore.collection<Incident>(
+      this.incidentListPath
+    );
+  }
+
+  async createIncident(incident: Incident) {
+    const id = this.firestore.createId();
+    return await this.incidentCollection.doc(id).set({ id, ...incident });
+  }
+
+  getAllIncidents() {
+    return this.incidentCollection.valueChanges();
+  }
+
+  getIncident(id: string) {
+    return this.incidentCollection.doc<Incident>(id).valueChanges();
+  }
+
+  async deleteIncident(id: string) {
+    await this.incidentCollection.doc(id).delete();
+  }
+}
