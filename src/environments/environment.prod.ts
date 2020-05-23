4cc02367832b207742c503fde1473bc1e0af58c8
Wade Fleming <WFleming@caci.co.uk>
2020-05-23 19:43:45 +0100

    add production environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: true,
+  appUrl: 'https://power-cut.web.app',
   googleApiKey: 'AIzaSyD2OiE51qs-H5nkHuUZFwJdnuWrVt5OZ0g', // restricted key
+  twitterHandle: 'PowerCutApp',
+  twitterShareMessage: 'I`ve just reported a power cut in my area',
 };
