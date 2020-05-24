1fe68123ad85d341c1ec46949c1a2a7901bec2de
Wade Fleming <WFleming@caci.co.uk>
2020-05-24 22:50:48 +0100

    use ion-search-bar instead of normal input

--- src/app/google-address-search/google-address-search.directive.ts
+++ src/app/google-address-search/google-address-search.directive.ts
@@ -1,13 +1,9 @@
-import {
-  Directive,
-  Output,
-  EventEmitter,
-  ElementRef,
-  NgZone,
-} from '@angular/core';
+import { Directive, Output, EventEmitter, NgZone } from '@angular/core';
 import { MapsAPILoader } from '@agm/core';
+import { IonSearchbar } from '@ionic/angular';
 
 // based on https://github.com/SebastianM/angular-google-maps/issues/467#issuecomment-342562167
+// adapted for ionic search bar
 @Directive({
   selector: '[appGoogleAddressSearch]',
 })
@@ -15,21 +11,21 @@ export class AppGoogleAddressSearchDirective {
   @Output() placeChanged = new EventEmitter<google.maps.places.PlaceResult>();
 
   constructor(
-    private el: ElementRef,
-    private ngZone: NgZone,
-    private mapsAPILoader: MapsAPILoader
+    ionSearchBar: IonSearchbar,
+    ngZone: NgZone,
+    mapsAPILoader: MapsAPILoader
   ) {
-    this.mapsAPILoader.load().then(() => {
-      const autocomplete = new google.maps.places.Autocomplete(
-        this.el.nativeElement
-      );
+    ionSearchBar.getInputElement().then((inputElement) =>
+      mapsAPILoader.load().then(() => {
+        const autocomplete = new google.maps.places.Autocomplete(inputElement);
 
-      autocomplete.addListener('place_changed', () => {
-        this.ngZone.run(() => {
-          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
-          this.placeChanged.emit(place);
+        autocomplete.addListener('place_changed', () => {
+          ngZone.run(() => {
+            const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+            this.placeChanged.emit(place);
+          });
         });
-      });
-    });
+      })
+    );
   }
 }
