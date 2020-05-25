9928b8ad098d917eef5766d766eba2cf11692182
Wade Fleming <WFleming@caci.co.uk>
2020-05-25 20:10:45 +0100

    bias places search to map bounds

--- src/app/google-address-search/google-address-search.directive.ts
+++ src/app/google-address-search/google-address-search.directive.ts
@@ -1,6 +1,14 @@
-import { Directive, Output, EventEmitter, NgZone, OnInit } from '@angular/core';
-import { MapsAPILoader } from '@agm/core';
+import {
+  Directive,
+  Output,
+  EventEmitter,
+  NgZone,
+  OnInit,
+  Input,
+} from '@angular/core';
+import { MapsAPILoader, AgmMap } from '@agm/core';
 import { IonSearchbar } from '@ionic/angular';
+import { first } from 'rxjs/operators';
 
 // based on https://github.com/SebastianM/angular-google-maps/issues/467#issuecomment-342562167
 // adapted for ionic search bar
@@ -8,6 +16,11 @@ import { IonSearchbar } from '@ionic/angular';
   selector: '[appGoogleAddressSearch]',
 })
 export class AppGoogleAddressSearchDirective implements OnInit {
+  private autocomplete: google.maps.places.Autocomplete;
+
+  @Input()
+  agmMap: AgmMap = null;
+
   @Output() placeChanged = new EventEmitter<google.maps.places.PlaceResult>();
 
   constructor(
@@ -19,15 +32,25 @@ export class AppGoogleAddressSearchDirective implements OnInit {
   ngOnInit() {
     this.ionSearchBar.getInputElement().then((inputElement) =>
       this.mapsAPILoader.load().then(() => {
-        const autocomplete = new google.maps.places.Autocomplete(inputElement);
+        this.autocomplete = new google.maps.places.Autocomplete(inputElement);
 
-        autocomplete.addListener('place_changed', () => {
+        this.autocomplete.addListener('place_changed', () => {
           this.ngZone.run(() => {
-            const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+            const place: google.maps.places.PlaceResult = this.autocomplete.getPlace();
             this.placeChanged.emit(place);
           });
         });
+
+        this.setupBiasToMapBounds();
       })
     );
+  }
+
+  setupBiasToMapBounds() {
+    this.agmMap.mapReady
+      .pipe(first())
+      .subscribe((gMap: google.maps.Map) =>
+        this.autocomplete.bindTo('bounds', gMap)
+      );
   }
 }
