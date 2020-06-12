d54f111991cca10ba2c9d39f61980ef3becf76d3
Wade Fleming <WFleming@caci.co.uk>
2020-06-12 23:12:51 +0100

    move functionality into new DomManipulator service

---
+++ src/app/services/dom-manipulator.service.ts
@@ -0,0 +1,58 @@
+import {
+  Injectable,
+  ComponentFactoryResolver,
+  ApplicationRef,
+  Injector,
+  EmbeddedViewRef,
+  ComponentRef,
+} from '@angular/core';
+import { NonModalOptions } from '../models/non-modal-options';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DomManipulator {
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    private appRef: ApplicationRef,
+    private injector: Injector
+  ) {}
+
+  appendComponentToBody(options: NonModalOptions) {
+    const { component, inputs } = { ...options };
+    // Create a component reference from the component
+    const componentRef = this.componentFactoryResolver
+      .resolveComponentFactory(component)
+      .create(this.injector);
+
+    if (inputs) {
+      Object.assign(componentRef.instance, inputs);
+    }
+
+    // Attach component to the appRef so that it's inside the ng component tree
+    this.appRef.attachView(componentRef.hostView);
+
+    // Get DOM element from component
+    const domElem = (componentRef.hostView as EmbeddedViewRef<any>)
+      .rootNodes[0] as HTMLElement;
+
+    // Get dialog element
+    const dialogElement = document.getElementById('non-modal-dialog-container');
+    if (!dialogElement) {
+      throw new Error(
+        'Ensure your layout contains an <app-non-modal-dialog> element'
+      );
+    }
+
+    // Append DOM element to the body
+    dialogElement.appendChild(domElem);
+
+    return componentRef;
+  }
+
+  removeComponentFromBody(componentRef: ComponentRef<unknown>) {
+    // remove it from the component tree and from the DOM
+    this.appRef.detachView(componentRef.hostView);
+    componentRef.destroy();
+  }
+}
--- src/app/services/non-modal-controller.service.ts
+++ src/app/services/non-modal-controller.service.ts
@@ -1,13 +1,7 @@
-import {
-  Injectable,
-  ComponentFactoryResolver,
-  ApplicationRef,
-  Injector,
-  EmbeddedViewRef,
-  ComponentRef,
-} from '@angular/core';
+import { Injectable, ComponentRef } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { NonModalOptions } from '../models/non-modal-options';
+import { DomManipulator } from './dom-manipulator.service';
 
 @Injectable({
   providedIn: 'root',
@@ -25,56 +19,16 @@ export class NonModalController {
   private activeSubject = new BehaviorSubject<boolean>(false);
   active$ = this.activeSubject.asObservable();
 
-  constructor(
-    private componentFactoryResolver: ComponentFactoryResolver,
-    private appRef: ApplicationRef,
-    private injector: Injector
-  ) {}
+  constructor(private domManipulator: DomManipulator) {}
 
   create(options: NonModalOptions) {
-    this.activeComponentRef = this.appendComponentToBody(options);
+    this.activeComponentRef = this.domManipulator.appendComponentToBody(
+      options
+    );
   }
 
   dismiss() {
-    this.removeComponentFromBody(this.activeComponentRef);
+    this.domManipulator.removeComponentFromBody(this.activeComponentRef);
     this.activeComponentRef = null;
-  }
-
-  private appendComponentToBody(options: NonModalOptions) {
-    const { component, inputs } = { ...options };
-    // Create a component reference from the component
-    const componentRef = this.componentFactoryResolver
-      .resolveComponentFactory(component)
-      .create(this.injector);
-
-    if (inputs) {
-      Object.assign(componentRef.instance, inputs);
-    }
-
-    // Attach component to the appRef so that it's inside the ng component tree
-    this.appRef.attachView(componentRef.hostView);
-
-    // Get DOM element from component
-    const domElem = (componentRef.hostView as EmbeddedViewRef<any>)
-      .rootNodes[0] as HTMLElement;
-
-    // Get dialog element
-    const dialogElement = document.getElementById('non-modal-dialog-container');
-    if (!dialogElement) {
-      throw new Error(
-        'Ensure your layout contains an <app-non-modal-dialog> element'
-      );
-    }
-
-    // Append DOM element to the body
-    dialogElement.appendChild(domElem);
-
-    return componentRef;
-  }
-
-  private removeComponentFromBody(componentRef: ComponentRef<unknown>) {
-    // remove it from the component tree and from the DOM
-    this.appRef.detachView(componentRef.hostView);
-    componentRef.destroy();
   }
 }
